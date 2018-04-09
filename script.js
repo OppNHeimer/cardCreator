648b924f88ed056636ea25b5e450c15db89bb8a5
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2018-04-09 14:38:47 -0400

    form created, able to upload and toggle images

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+function addImages() {
+  let gallery = document.querySelector('.gallery')
+  let files    = document.querySelector('input[type=file]').files
+
+  function readAndAdd(file) {
+    
+    var reader  = new FileReader()
+    reader.addEventListener("load", function () {
+      let image = new Image()
+      image.title = file.name
+      image.height  = 100;
+      image.src = this.result
+      image.onclick = (e) => {togglePreview(e.target.src)}
+      gallery.appendChild(image)
+      togglePreview(this.result)
+    }, false)
+
+    reader.readAsDataURL(file)
+  }
+
+  if (files) {
+    Array.from(files).forEach( function(file) { readAndAdd(file) })
+  }
+}
+
+function togglePreview (src) {
+  let previewImage = document.querySelector('.preview__img')
+  previewImage.src = src
+}
+
+window.onload = () => {
+  console.log('load')
+}
